60b83347fa39f144699f28685a03896397e4cb47
yorkspy@gmail.com <yorkspy@gmail.com>
2020-12-12 11:14:50 -0500

    Added loading bar and finished up the game

---
+++ Assets/The Great Fleece/Game/Scripts/Menu/LoadLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour
+{
+    [SerializeField] private Image _progressBar;
+
+    private void Start()
+    {
+        StartCoroutine(LoadLevelAsync());
+    }
+
+    IEnumerator LoadLevelAsync()
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("Main");
+
+        while (asyncOperation.isDone == false)
+        {
+            _progressBar.fillAmount = asyncOperation.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
--- Assets/The Great Fleece/Game/Scripts/Menu/MainMenu.cs
+++ Assets/The Great Fleece/Game/Scripts/Menu/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("LoadingScreen");
     }
 
     public void QuitGame()
